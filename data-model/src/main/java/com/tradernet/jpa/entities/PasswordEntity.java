5fbdc65af9a39a41c6bd71a5dffeb248cb78bb0d
Carl Scrivener <carl.scrivener.89@gmail.com>
2026-02-08 17:50:21 +0000

    Persist password hash as string field

--- data-model/src/main/java/com/tradernet/jpa/entities/PasswordEntity.java
+++ data-model/src/main/java/com/tradernet/jpa/entities/PasswordEntity.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 import java.util.Date;
 
 /**
@@ -27,7 +28,10 @@ public class PasswordEntity {
 
     private Date lastChanged;
 
+    @Transient
     private PasswordHash password;
+
+    private String passwordHash;
 
     public PasswordEntity() {
     }
@@ -35,12 +39,14 @@ public class PasswordEntity {
     public PasswordEntity(UserEntity user, PasswordHash password) {
         this.user = user;
         this.password = password;
+        this.passwordHash = password == null ? null : password.toString();
         this.lastChanged = new Date();
     }
 
     public PasswordEntity(PasswordEntity existingPassword, PasswordHash updatedPassword) {
         this.user = existingPassword.user;
         this.password = updatedPassword;
+        this.passwordHash = updatedPassword == null ? null : updatedPassword.toString();
         this.lastChanged = new Date();
     }
 
@@ -49,6 +55,9 @@ public class PasswordEntity {
     }
 
     public PasswordHash getPassword() {
+        if (password == null && passwordHash != null) {
+            password = new com.tradernet.jpa.databaseservice.passwords.PBKDF2PasswordHash(passwordHash);
+        }
         return password;
     }
 
@@ -70,5 +79,17 @@ public class PasswordEntity {
 
     public void setPassword(PasswordHash password) {
         this.password = password;
+        this.passwordHash = password == null ? null : password.toString();
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+        this.password = passwordHash == null
+            ? null
+            : new com.tradernet.jpa.databaseservice.passwords.PBKDF2PasswordHash(passwordHash);
     }
 }
