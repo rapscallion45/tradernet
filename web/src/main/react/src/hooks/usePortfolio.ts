44bc202ca886a35de0cc17c0e1267bc86c50879b
Carl Scrivener <carl.scrivener.89@gmail.com>
2026-04-24 22:55:16 +0100

    Fix portfolio hook to use existing currency preference hook

--- web/src/main/react/src/hooks/usePortfolio.ts
+++ web/src/main/react/src/hooks/usePortfolio.ts
@@ -2,13 +2,13 @@ import { useSuspenseQuery, UseSuspenseQueryResult } from "@tanstack/react-query"
 import { getRestClient } from "api/RestClient"
 import { PortfolioSummary } from "api/types"
 import { QueryClientKeys } from "global/constants"
-import { useActiveCurrency } from "hooks/useActiveCurrency"
+import { useCurrencyPreference } from "hooks/useCurrencyPreference"
 
 /**
  * Returns authenticated user's portfolio summary.
  */
 export const usePortfolio = (): UseSuspenseQueryResult<PortfolioSummary> => {
-  const currency = useActiveCurrency()
+  const { currency } = useCurrencyPreference()
 
   return useSuspenseQuery({
     queryKey: [QueryClientKeys.Portfolio, currency],
